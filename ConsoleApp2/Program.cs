0e58d1b9b3f080eebd68bdfdd71aac6511409939
ASUS <ASUS@LAPTOP-SK13NBV6>
2025-09-09 18:32:56 +0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            while (true) {
+                students.Add(QLHS.Nhap1HS());
+                Console.WriteLine("ban co muon nhap tiep khong (1(co)/(any)(khong)");
+                var input = Console.ReadLine();
+                if (input != "1") break;
+
+            }
+            Console.WriteLine("Danh sach hoc sinh vua nhap la: ");
+            students.ForEach(i => QLHS.In1HS(i));
+            QLHS.Menu();
+
+            do
+            {
+                Console.WriteLine("Moi ban chon hanh dong muon thuc hien");
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        QLHS.InHS15den18(students);
+                        break;
+                    case "2":
+                        QLHS.InHSTenBatDauBangA(students);
+                        break;
+                    case "3":
+                        QLHS.InTongSoTuoi(students);
+                        break;
+                    case "4":
+                        QLHS.InHSLonNhat(students);
+                        break;
+                    case "5":
+                        QLHS.SapXepTheoTuoi(students);
+                        break;
+                    case "7":
+                        QLHS.Them20SV(students);
+                        Console.WriteLine("Danh sach hoc sinh sau khi them 20 sinh vien la: ");
+                        students.ForEach(i => QLHS.In1HS(i));
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Lua chon khong hop le, vui long chon lai");
+                        break;
+                }
+                QLHS.Menu();
+            } while (true);
+
+        }
+    } 
+}
---
+++ ConsoleApp2/QLHS.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class QLHS
+    {
+        public static void Menu()
+        {
+            Console.WriteLine("1. In cac hoc sinh co tuoi tu 15 den 18");
+            Console.WriteLine("2. In cac hoc sih co ten bat dau bang chu A");
+            Console.WriteLine("3. In ong so tuoi cua tat ca hoc sinh trong danh sach");
+            Console.WriteLine("4. In hoc sinh co tuoi lon nhat");
+            Console.WriteLine("5. Sap xep danh sach hoc sinh theo tuoi tang dan");
+            Console.WriteLine("7. them 20 sinh vien ngau nhien vao danh sach");
+            Console.WriteLine("0. Thoat");
+        }
+        public static Student Nhap1HS()
+        {
+            Student student = new Student();
+            Console.Write("Nhap Id: ");
+            student.Id = int.Parse(Console.ReadLine());
+            Console.Write("Nhap Ten: ");
+            student.Name = Console.ReadLine();
+            Console.Write("Nhap Tuoi: ");
+            student.Age = int.Parse(Console.ReadLine());
+            return student;
+        }
+        public static void In1HS(Student student)
+        {
+            Console.WriteLine(student.Id + "---" + student.Name + "---" + student.Age);
+        }
+        public static void InHS15den18(List<Student> students)
+        {
+            students.Where(s => s.Age >= 15 && s.Age <= 18).ToList().ForEach(s => In1HS(s));
+        }
+        public static void InHSTenBatDauBangA(List<Student> students)
+        {
+            students.Where(s => s.Name.StartsWith("A")).ToList().ForEach(s => In1HS(s));
+        }
+        public static void InTongSoTuoi(List<Student> students)
+        {
+            Console.WriteLine("Tong so tuoi cua tat ca hoc sinh la: " + students.Sum(s => s.Age));
+        }
+        public static void InHSLonNhat(List<Student> students)
+        {
+            int maxAge = students.Max(studient => studient.Age);
+            students.Where(s => s.Age == maxAge).ToList().ForEach(s => In1HS(s));
+        }
+        public static void SapXepTheoTuoi(List<Student> students)
+        {
+            students.OrderBy(s => s.Age).ToList().ForEach(s => In1HS(s));
+        }
+        public static void Them20SV(List<Student> students)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                Student student = new Student();
+                student.Id = i + 1;
+                student.Name =(char) rand.Next('A','D'+1) + "Student" + (i + 1);
+                student.Age = rand.Next(10, 21); // Tuoi ngau nhien tu 10 den 20
+                students.Add(student);
+            }
+        }
+
+
+
+    }
+}// End of ConsoleApp2/QLHS.cs
